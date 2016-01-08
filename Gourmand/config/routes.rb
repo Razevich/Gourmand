1b91acea65260ee566297b71ccc5a3814c4ab5f8
Razevich <razevich.c@gmail.com>
2016-01-08 15:04:43 -0600

    add routes for recipe

--- Gourmand/config/routes.rb
+++ Gourmand/config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "/user/:id" => "user#show",              defaults: {format: :json}
   get "/user" => "user#new",                   defaults: {format: :json}
   post "/user" => "user#create",               defaults: {format: :json}
+  get "/recipe" => "recipe#new",               defaults: {format: :json}
+  post "/recipe" => "recipe#create",           defaults: {format: :json}
+  get "/recipe/:id" => "recipe#show",          defaults: {format: :json}
 end
