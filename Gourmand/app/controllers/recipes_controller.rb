e67980ae184bfe70ffbd8eea910bfa459380935a
Razevich <razevich.c@gmail.com>
2016-01-08 15:26:22 -0600

    add recipe controller

--- Gourmand/app/controllers/recipes_controller.rb
+++ Gourmand/app/controllers/recipes_controller.rb
@@ -1,2 +1,14 @@
 class RecipeController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(user_id: current_user.id, name: params[:name])
+  end
+
+  def show
+    @recipe = Recipe.find_by(params[:id])
+  end
 end
