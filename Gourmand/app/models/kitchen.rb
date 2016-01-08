ce26656237d6bcbfbfed77b8f35785afea2dedad
Razevich <razevich.c@gmail.com>
2016-01-08 14:43:16 -0600

    add generate shopping_list for kitchens

--- Gourmand/app/models/kitchen.rb
+++ Gourmand/app/models/kitchen.rb
@@ -4,4 +4,9 @@ class Kitchen < ActiveRecord::Base
   has_many :shopping_lists
   has_many :cook_books
   belongs_to :creator, class_name: "User"
+  after_save :add_shopping_list
+
+  def add_shopping_list
+    self.shopping_lists.create(kitchen_id: self.id)
+  end
 end
--- Gourmand/db/migrate/20160108164924_create_shopping_lists.rb
+++ Gourmand/db/migrate/20160108164924_create_shopping_lists.rb
@@ -2,6 +2,7 @@ class CreateShoppingLists < ActiveRecord::Migration
   def change
     create_table :shopping_lists do |t|
       t.integer  :kitchen_id
+      t.string   :name
 
       t.timestamps null: false
     end
