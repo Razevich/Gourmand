a4b1877a61b58ed0ea5aa01363d887570df86514
Razevich <razevich.c@gmail.com>
2016-01-09 14:32:04 -0600

    add more routes and controllers

--- app/controllers/cook_books_controller.rb
+++ app/controllers/cook_books_controller.rb
@@ -5,4 +5,10 @@ class CookBookController < ApplicationController
     @cook_book = CookBook.find_by(id: params[:id])
     render json: @cook_book, status: :ok
   end
+
+  def destroy
+    @cook_book = CookBook.find_by(id: params[:id])
+    @cook_book.destroy
+    head: :no_content #204 code, no content. This means it was good.
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new", as: "root",                     defaults: {format: :json}
   post "/login" => "sessions#create",                             defaults: {format: :json}
   delete "/logout" => "sessions#destroy",                         defaults: {format: :json}
+
   get "/user/:id" => "users#show",                                defaults: {format: :json}
   get "/user" => "users#new",                                     defaults: {format: :json}
   post "/user" => "users#create",                                 defaults: {format: :json}
+
+  get "/recipe/id" => "recipes#show",                             defaults: {format: :json}
   post "/recipe/:recipe_id/steps" => "recipes#steps",             defaults: {format: :json}
-  post "/recipe/:recipe_id/notes" => "notes#recipes",             defaults: {format: :json}
-  post "/step/:step_id/notes" => "notes#steps",                   defaults: {format: :json}
-  get "/cook_book/:id" => "cook_books#show",                      defualts: {format: :json}
+
+  get "/cook_book/:id" => "cook_books#show",                      defaults: {format: :json}
   post "/cook_book/:cook_book_id/recipe" => "recipes#create",     defaults: {format: :json}
   delete "/cook_book/:id" => "cook_books#destroy",                defaults: {format: :json}
+
+  get "/note/:id" => "notes#show",                                defaults: {format: :json}
+  post "/recipe/:recipe_id/notes" => "notes#recipes",             defaults: {format: :json}
+  post "/step/:step_id/notes" => "notes#steps",                   defaults: {format: :json}
 end
