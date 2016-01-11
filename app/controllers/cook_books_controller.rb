8a99bbfdb48ae821389d3997aa416743ebb4b6a6
Farman Pirzada <farmanp.home@gmai.com>
2016-01-11 14:02:59 -0600

    Create show renders for json in cookbooks

--- app/controllers/cook_books_controller.rb
+++ app/controllers/cook_books_controller.rb
@@ -4,6 +4,7 @@ class CookBooksController < ApplicationController
   def show
     @cook_book = CookBook.find_by(id: params[:id])
     render json: @cook_book.recipes
+    render json: @cook_book
     # , status: :ok
   end
 
