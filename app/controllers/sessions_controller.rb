86e894a60bef679460ff1e9d645cb914d3aa9129
Farman Pirzada <farmanp.home@gmai.com>
2016-01-11 15:37:01 -0600

    Remove destroy method sessions and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,19 +7,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.user_id
 
       render json: @user
       render json: @user.kitchens
       # status: :ok
     else
-      render json: "Invalid username or password"
+      render json: "Nope"
       # status: :unprocessable_entity # error 422 code
     end
   end
 
   def destroy
-    session.delete(:user_id)
+
     # status: :no_content #204 code
   end
 
