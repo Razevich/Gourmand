62cfff7c99445e95c5684cf2f84de835f2cc49ed
Farman Pirzada <farmanp.home@gmai.com>
2016-01-11 15:41:32 -0600

    Test params in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    p params
     @user = User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password])
-
+      p params
       render json: @user
       render json: @user.kitchens
       # status: :ok
     else
+      p params
       render json: "Nope"
       # status: :unprocessable_entity # error 422 code
     end
