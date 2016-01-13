9dcd4c09759ea227be952ebb247e1cf5d7c65957
Farman Pirzada <farmanp.home@gmai.com>
2016-01-13 13:27:20 -0600

    Add search method in kitchen model

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -6,6 +6,20 @@ class Kitchen < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   after_save :add_shopping_list, :add_cook_book
 
+  def index
+    if params[:search]
+      @kitchens = Kitchen.search(params[:search]).order("created_at DESC")
+    else
+      @kitchens = []
+    end
+  end
+
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    where("name like ?", "%#{query}%")
+    render :json => {kitchens: @kitchens}
+  end
+
   def add_shopping_list
     self.shopping_lists.create(kitchen_id: self.id)
   end
