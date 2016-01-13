e373b8c99d4dd17746aec8a256f9f3f7caab6e60
Farman Pirzada <farmanp.home@gmai.com>
2016-01-13 13:54:16 -0600

    Fix index in migration tables

--- db/migrate/20160113194446_add_index_to_kitchen.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToKitchen < ActiveRecord::Migration
-  def change
-    add_index :kitchen, :name
-  end
-end
--- db/migrate/20160113194738_add_index_to_recipe.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToRecipe < ActiveRecord::Migration
-  def change
-    add_index :recipe, :name
-  end
-end
