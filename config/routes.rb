18966431eddedcd94c4f41c533fee50a87e97bb5
Farman Pirzada <farmanp.home@gmai.com>
2016-01-13 10:55:30 -0600

    Come back to backend testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-
   get "/login" => "sessions#new", as: "root",                     defaults: {format: :json}
   post "/login" => "sessions#create",                             defaults: {format: :json}
   delete "/logout" => "sessions#destroy",                         defaults: {format: :json}
+
+  get "/users" => "users#index",                                  defaults: {format: :json}
 
   get "/user/:id" => "users#show",                                defaults: {format: :json}
   get "/user" => "users#new",                                     defaults: {format: :json}
