aa42629ffb08dce674ca1e5a5944486f1c3cd17a
Razevich <razevich.c@gmail.com>
2016-01-13 17:31:04 -0600

    add ids for recipes

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -13,8 +13,10 @@ class KitchensController < ApplicationController
     @users = @kitchen.users
     @usernames = []
     @recipe_names = []
+    @recipe_ids = []
     @kitchen.cook_books.first.recipes.each do |recipe|
       @recipe_names << recipe.name
+      @recipe_ids << recipe.id
     end
 
     @users.each do |user|
