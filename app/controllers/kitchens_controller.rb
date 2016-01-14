4e09ce407bd2e6375322c8403864ed3077b0511e
Farman Pirzada <farmanp.home@gmai.com>
2016-01-14 09:00:05 -0600

    f

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -30,7 +30,6 @@ class KitchensController < ApplicationController
       if @kitchen.save
         current_user.kitchens << @kitchen
         current_user.save
-        @kitchen << current_user
         render :json => {kitchen: @kitchen, user_token: current_user.token}
       else
         @errors = errors.full_messages
